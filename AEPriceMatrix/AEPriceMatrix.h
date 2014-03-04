1c0303608b4771eeddd050fd71f04db093353fb9
Christian Wellenbrock <christian.wellenbrock@gmail.com>
2014-03-04 15:38:01 +0100

    Add class AEPriceMatrix

---
+++ AEPriceMatrix/AEPriceMatrix.h
@@ -0,0 +1,13 @@
+//
+//  AEPriceMatrix.h
+//  AEPriceMatrix
+//
+//  Created by Christian Wellenbrock on 04.03.14.
+//  Copyright (c) 2014 adeven. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface AEPriceMatrix : NSObject
+
+@end
---
+++ AEPriceMatrix/AEPriceMatrix.m
@@ -0,0 +1,13 @@
+//
+//  AEPriceMatrix.m
+//  AEPriceMatrix
+//
+//  Created by Christian Wellenbrock on 04.03.14.
+//  Copyright (c) 2014 adeven. All rights reserved.
+//
+
+#import "AEPriceMatrix.h"
+
+@implementation AEPriceMatrix
+
+@end
