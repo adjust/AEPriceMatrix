14d7388dc68cf6d31e50d6798d2a82c315a1f830
Christian Wellenbrock <christian.wellenbrock@gmail.com>
2014-03-04 18:52:41 +0100

    Add private lazy loading [AEPriceMatrix priceMatrix]

--- AEPriceMatrix/AEPriceMatrix.m
+++ AEPriceMatrix/AEPriceMatrix.m
@@ -8,6 +8,33 @@
 
 #import "AEPriceMatrix.h"
 
+static NSDictionary *priceMatrix = nil;
+
+
+@interface AEPriceMatrix(private)
+
++ (NSDictionary *)priceMatrix;
+
+@end
+
+
+#pragma mark -
 @implementation AEPriceMatrix
+
+@end
+
+
+#pragma mark -
+@implementation AEPriceMatrix(private)
+
+// lazily read price matrix from plist file
++ (NSDictionary *)priceMatrix {
+    if (priceMatrix == nil) {
+        NSBundle *bundle = [NSBundle mainBundle];
+        NSString *path = [bundle pathForResource:@"AEPriceMatrix" ofType:@"plist"];
+        priceMatrix = [NSDictionary dictionaryWithContentsOfFile:path];
+    }
+    return priceMatrix;
+}
 
 @end
